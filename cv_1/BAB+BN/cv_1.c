8b5fcb3c09ce8a2c4f129f52db3ecf3c09d5cb04
danielmatosfalc <danielmatosfalc@gmail.com>
2026-03-26 16:24:56 -0300

    Heurísticas construtivas parte final

---
+++ cv_1/BAB+BN/cv_1.c
@@ -0,0 +1,277 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "cv_1.h"
+#include<math.h>
+#include<float.h>
+#include <ncurses.h>
+
+//Teremos a função gulosa que usa a heurística de Bellmore e Nemhauser para inicializar, e usa branch and bound para podar todas que forem maior do que o valor dela
+//Bellmore e Nemhauser
+
+struct cidade{
+
+    double latitude, longitude;
+    int visitado;
+
+};
+
+
+
+int main(){
+
+    Cidade *vetorCidades = malloc(CIDADES* sizeof(Cidade));
+    int *caminho = malloc(CIDADES * sizeof(int));
+    int *resultadoFinal = malloc(CIDADES * sizeof(int));
+
+    //Apenas lerei os valores e colocarei o identificador em cada Cidade
+    for(int i = 0; i < CIDADES; i++){
+        resultadoFinal[i] = 0;
+        caminho[i] = -1;
+        vetorCidades[i].visitado = 0;
+        scanf("%lf", &vetorCidades[i].latitude);
+        scanf("%lf", &vetorCidades[i].longitude);
+    }
+
+    double **matrizDistancia = malloc(CIDADES * sizeof(double*));
+    for(int i = 0; i < CIDADES; i++){
+
+        matrizDistancia[i] = malloc(CIDADES * sizeof(double));
+        for(int j = 0; j < CIDADES; j++){
+
+            matrizDistancia[i][j] = distanciaEuclidiana(vetorCidades[i].latitude, vetorCidades[j].latitude, vetorCidades[i].longitude, vetorCidades[j].longitude);
+
+
+        }
+    }
+
+    //terei que fixar a primeira posição da cidade, por questões de complexidad
+    caminho[0] = 0;
+    vetorCidades[0].visitado = 1;
+    double melhorCusto = heuristicaBN(vetorCidades, matrizDistancia, resultadoFinal);
+
+    for(int i = 1; i < CIDADES; i++){
+        vetorCidades[i].visitado = 0;
+    }
+
+    branchAndBound(vetorCidades, 1, caminho, 0, matrizDistancia, &melhorCusto, resultadoFinal);
+    printf("Menor distancia encontrada: %.6lf\n", melhorCusto);
+    printf("Caminho: ");
+    for (int i = 0; i < CIDADES; i++) {
+        printf("%d ", resultadoFinal[i]);
+    }
+    printf("\n");
+
+
+
+
+
+    free(resultadoFinal);
+    free(caminho);
+    free(vetorCidades);
+    for(int i = 0; i < CIDADES; i++){
+
+        free(matrizDistancia[i]);
+
+    }
+    free(matrizDistancia);
+    return 0;
+
+}
+
+
+double distanciaEuclidiana(double x1, double y1, double x2, double y2){
+
+    double  resposta;
+
+
+    resposta = (pow((x1 - y1), 2)) + (pow((x2 - y2), 2));
+    resposta = sqrt(resposta);
+
+
+
+    return resposta;
+}
+
+//melhor custo deve ser iniciado com um valor muito alto
+void branchAndBound(Cidade *vetorCidades, int nivel, int caminho[], double custoAtual, double **matrizDistancia, double *melhorCusto, int resultadoFinal[]){
+
+    if(nivel == CIDADES){
+        double custoTotal = custoAtual + matrizDistancia[caminho[nivel - 1]][caminho[0]];
+
+        if(custoTotal < *melhorCusto){
+            *melhorCusto = custoTotal;
+
+            for(int i = 0; i < CIDADES; i++){
+                resultadoFinal[i] = caminho[i];
+            }
+        }
+
+        return;
+    }
+
+    for(int i = 1; i < CIDADES; i++){
+
+
+        //Se a cidade já não está no caminho
+        if(vetorCidades[i].visitado == 0){
+
+            double novoCusto = custoAtual + matrizDistancia[caminho[nivel -1]][i];
+
+            caminho[nivel] = i;
+            vetorCidades[i].visitado = 1;
+            double limite = calcularLimiteInferior(vetorCidades, caminho, nivel + 1, novoCusto, matrizDistancia);
+            if(limite <  *melhorCusto){
+                branchAndBound(vetorCidades, nivel + 1, caminho, novoCusto, matrizDistancia, melhorCusto, resultadoFinal);
+            }
+
+            vetorCidades[i].visitado = 0;
+            caminho[nivel] = -1;
+
+        }
+
+
+
+
+    }
+
+
+}
+
+double calcularLimiteInferior(Cidade *vetorCidades, int caminho[], int nivel, double custoAtual, double **matrizDistancia) {
+    double limite = custoAtual;
+
+    int ultimaCidade = caminho[nivel - 1];
+
+    // menor saída da última cidade para alguma não visitada
+    double menorSaidaUltima = DBL_MAX;
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            if(matrizDistancia[ultimaCidade][i] < menorSaidaUltima){
+                menorSaidaUltima = matrizDistancia[ultimaCidade][i];
+            }
+        }
+    }
+
+    if(menorSaidaUltima != DBL_MAX){
+        limite += menorSaidaUltima;
+    }
+
+    // para cada cidade não visitada, soma sua menor saída
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            double menor = DBL_MAX;
+
+            for(int j = 0; j < CIDADES; j++){
+                if(i != j){
+                    if(matrizDistancia[i][j] < menor){
+                        menor = matrizDistancia[i][j];
+                    }
+                }
+            }
+
+            limite += menor;
+        }
+    }
+
+    // retorno à origem (estimativa simples)
+    double menorRetorno = DBL_MAX;
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            if(matrizDistancia[i][caminho[0]] < menorRetorno){
+                menorRetorno = matrizDistancia[i][caminho[0]];
+            }
+        }
+    }
+
+    //Não estou considerando o retorno, porque pode levar a um valor acima do ideal, visto que eu estou somando mais acima do que o ideal
+
+    if(menorRetorno != DBL_MAX){
+       // limite += menorRetorno;
+    }
+
+    return limite;
+}
+
+
+double heuristicaBN(Cidade *vetorCidades, double **matrizDistancia, int *resultadoFinal){
+
+    int cont = 0, nivel = 0;
+    double soma = 0;
+    resultadoFinal[0] = 0;
+    while(cont < CIDADES -1){
+
+        int extremo = 0;
+        int cidade = -1;
+        double menorDist = DBL_MAX;
+
+        for(int j = 1; j < CIDADES; j++){
+
+            if(vetorCidades[j].visitado == 0){
+            
+                if(matrizDistancia[resultadoFinal[0]][j] < menorDist){
+
+                    menorDist = matrizDistancia[resultadoFinal[0]][j];
+                    cidade = j;
+                }
+
+
+            }
+
+        }
+
+        if(nivel >= 1){
+
+            
+            for(int j =1; j < CIDADES; j++){
+
+                if( vetorCidades[j].visitado == 0){
+                
+                    if(matrizDistancia[resultadoFinal[nivel]][j] < menorDist){
+
+                        menorDist = matrizDistancia[resultadoFinal[nivel]][j];
+                        cidade = j;
+                        extremo = 1;
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        if(cidade == -1){
+            break;
+        }
+
+
+        if(extremo == 1 || nivel == 0){
+
+            nivel++;
+            resultadoFinal[nivel] = cidade;
+        }else{
+            empurrarVet(resultadoFinal, nivel);
+            resultadoFinal[0] = cidade;
+            nivel++;
+        }
+        
+        soma += menorDist;
+
+        vetorCidades[cidade].visitado = 1;
+
+        cont++;
+    }
+    //tem que somar a volta
+    soma += matrizDistancia[resultadoFinal[nivel]][resultadoFinal[0]];
+    return soma;
+}
+
+void empurrarVet(int* resultadoFinal, int nivel){
+
+    for(int i =nivel + 1; i > 0; i++){
+
+        resultadoFinal[i] = resultadoFinal[i-1];
+
+    }
+
+}
---
+++ cv_1/BAB+IB/cv_1.c
@@ -0,0 +1,303 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "cv_1.h"
+#include<math.h>
+#include<float.h>
+#include <ncurses.h>
+
+//Teremos a função gulosa que usa a heurística de Bellmore e Nemhauser para inicializar, e usa branch and bound para podar todas que forem maior do que o valor dela
+//Bellmore e Nemhauser
+
+struct cidade{
+
+    double latitude, longitude;
+    int visitado;
+
+};
+
+
+
+int main(){
+
+    Cidade *vetorCidades = malloc(CIDADES* sizeof(Cidade));
+    int *caminho = malloc(CIDADES * sizeof(int));
+    int *resultadoFinal = malloc(CIDADES * sizeof(int));
+
+    //Apenas lerei os valores e colocarei o identificador em cada Cidade
+    for(int i = 0; i < CIDADES; i++){
+        resultadoFinal[i] = 0;
+        caminho[i] = -1;
+        vetorCidades[i].visitado = 0;
+        scanf("%lf", &vetorCidades[i].latitude);
+        scanf("%lf", &vetorCidades[i].longitude);
+    }
+
+    double **matrizDistancia = malloc(CIDADES * sizeof(double*));
+    for(int i = 0; i < CIDADES; i++){
+
+        matrizDistancia[i] = malloc(CIDADES * sizeof(double));
+        for(int j = 0; j < CIDADES; j++){
+
+            matrizDistancia[i][j] = distanciaEuclidiana(vetorCidades[i].latitude, vetorCidades[j].latitude, vetorCidades[i].longitude, vetorCidades[j].longitude);
+
+
+        }
+    }
+
+    //terei que fixar a primeira posição da cidade, por questões de complexidad
+    caminho[0] = 0;
+    vetorCidades[0].visitado = 1;
+    double melhorCusto = heuristicaInsercaoBarata(vetorCidades, matrizDistancia, resultadoFinal);
+
+    for(int i = 1; i < CIDADES; i++){
+        vetorCidades[i].visitado = 0;
+    }
+
+    branchAndBound(vetorCidades, 1, caminho, 0, matrizDistancia, &melhorCusto, resultadoFinal);
+    printf("Menor distancia encontrada: %.6lf\n", melhorCusto);
+    printf("Caminho: ");
+    for (int i = 0; i < CIDADES; i++) {
+        printf("%d ", resultadoFinal[i]);
+    }
+    printf("\n");
+
+
+
+
+
+    free(resultadoFinal);
+    free(caminho);
+    free(vetorCidades);
+    for(int i = 0; i < CIDADES; i++){
+
+        free(matrizDistancia[i]);
+
+    }
+    free(matrizDistancia);
+    return 0;
+
+}
+
+
+double distanciaEuclidiana(double x1, double y1, double x2, double y2){
+
+    double  resposta;
+
+
+    resposta = (pow((x1 - y1), 2)) + (pow((x2 - y2), 2));
+    resposta = sqrt(resposta);
+
+
+
+    return resposta;
+}
+
+//melhor custo deve ser iniciado com um valor muito alto
+void branchAndBound(Cidade *vetorCidades, int nivel, int caminho[], double custoAtual, double **matrizDistancia, double *melhorCusto, int resultadoFinal[]){
+
+    if(nivel == CIDADES){
+        double custoTotal = custoAtual + matrizDistancia[caminho[nivel - 1]][caminho[0]];
+
+        if(custoTotal < *melhorCusto){
+            *melhorCusto = custoTotal;
+
+            for(int i = 0; i < CIDADES; i++){
+                resultadoFinal[i] = caminho[i];
+            }
+        }
+
+        return;
+    }
+
+    for(int i = 1; i < CIDADES; i++){
+
+
+        //Se a cidade já não está no caminho
+        if(vetorCidades[i].visitado == 0){
+
+            double novoCusto = custoAtual + matrizDistancia[caminho[nivel -1]][i];
+
+            caminho[nivel] = i;
+            vetorCidades[i].visitado = 1;
+            double limite = calcularLimiteInferior(vetorCidades, caminho, nivel + 1, novoCusto, matrizDistancia);
+            if(limite <  *melhorCusto){
+                branchAndBound(vetorCidades, nivel + 1, caminho, novoCusto, matrizDistancia, melhorCusto, resultadoFinal);
+            }
+
+            vetorCidades[i].visitado = 0;
+            caminho[nivel] = -1;
+
+        }
+
+
+
+
+    }
+
+
+}
+
+double calcularLimiteInferior(Cidade *vetorCidades, int caminho[], int nivel, double custoAtual, double **matrizDistancia) {
+    double limite = custoAtual;
+
+    int ultimaCidade = caminho[nivel - 1];
+
+    // menor saída da última cidade para alguma não visitada
+    double menorSaidaUltima = DBL_MAX;
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            if(matrizDistancia[ultimaCidade][i] < menorSaidaUltima){
+                menorSaidaUltima = matrizDistancia[ultimaCidade][i];
+            }
+        }
+    }
+
+    if(menorSaidaUltima != DBL_MAX){
+        limite += menorSaidaUltima;
+    }
+
+    // para cada cidade não visitada, soma sua menor saída
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            double menor = DBL_MAX;
+
+            for(int j = 0; j < CIDADES; j++){
+                if(i != j){
+                    if(matrizDistancia[i][j] < menor){
+                        menor = matrizDistancia[i][j];
+                    }
+                }
+            }
+
+            limite += menor;
+        }
+    }
+
+    // retorno à origem (estimativa simples)
+    double menorRetorno = DBL_MAX;
+    for(int i = 0; i < CIDADES; i++){
+        if(vetorCidades[i].visitado == 0){
+            if(matrizDistancia[i][caminho[0]] < menorRetorno){
+                menorRetorno = matrizDistancia[i][caminho[0]];
+            }
+        }
+    }
+
+    //Não estou considerando o retorno, porque pode levar a um valor acima do ideal, visto que eu estou somando mais acima do que o ideal
+
+    if(menorRetorno != DBL_MAX){
+       // limite += menorRetorno;
+    }
+
+    return limite;
+}
+
+
+double heuristicaInsercaoBarata(Cidade *vetorCidades, double **matrizDistancia, int *resultadoFinal){
+
+    for(int i = 0; i < CIDADES; i++){
+
+        resultadoFinal[i] = -1;
+
+    }
+
+
+    //Como nesse código eu preciso de um ciclo mínimo, no caso de 2 cidades, eu vou pegar a primeira e também a mais próxima da primeira
+    resultadoFinal[0] = 0;
+    vetorCidades[0].visitado = 1;
+    double menosDist = matrizDistancia[0][1];
+    int cidade = 1;
+    for(int i = 2; i < CIDADES; i++){
+
+        if(matrizDistancia[0][i] < menosDist){
+
+            menosDist = matrizDistancia[0][i];
+            cidade = i;
+
+        }
+
+
+    }
+    vetorCidades[cidade].visitado = 1;
+    resultadoFinal[1] = cidade;
+    int tamCiclo = 2;
+
+    //Enquanto o ciclo for menor que o numero de cidades total
+    while(tamCiclo < CIDADES){
+
+        int melhorCidade = -1;
+        int melhorPosicao = -1;
+        double melhorDelta = DBL_MAX;
+
+        for(int k = 0; k < CIDADES; k++){
+
+            if(vetorCidades[k].visitado == 0){
+
+
+
+                for(int pos = 0; pos < tamCiclo; pos++){
+
+                    int i = resultadoFinal[pos];
+                    int j;
+
+                    if(pos == tamCiclo - 1){
+
+                        j = resultadoFinal[0];
+
+                    }else{
+
+                        j = resultadoFinal[pos+1];
+
+                    }
+
+                    //Eu vou inserir entre o i e o j, nesse caso eu estou tentando calcular o acréscimo da distância, subtraindo pela distancia
+                    //entre o i e o j, que era o que estava antes
+                    double delta = matrizDistancia[i][k] + matrizDistancia[k][j] - matrizDistancia[i][j];
+
+                    if(delta < melhorDelta){
+
+                        melhorDelta = delta;
+                        melhorCidade = k;
+                        melhorPosicao = pos;
+
+                    }
+
+                }
+
+
+            }
+
+        }
+        for(int m = tamCiclo; m > melhorPosicao + 1; m--){
+            resultadoFinal[m] = resultadoFinal[m - 1];
+        }
+
+        resultadoFinal[melhorPosicao + 1] = melhorCidade;
+        vetorCidades[melhorCidade].visitado = 1;
+        tamCiclo++;
+
+    }
+
+    double soma = 0;
+
+    for(int i = 0; i < CIDADES -1; i++){
+
+        soma += matrizDistancia[resultadoFinal[i]][resultadoFinal[i + 1]];
+
+
+    }
+    
+    //tem que somar a volta
+    soma += matrizDistancia[resultadoFinal[CIDADES - 1]][resultadoFinal[0]];
+    return soma;
+}
+
+void empurrarVet(int* resultadoFinal, int nivel){
+
+    for(int i =nivel + 1; i > 0; i++){
+
+        resultadoFinal[i] = resultadoFinal[i-1];
+
+    }
+
+}
